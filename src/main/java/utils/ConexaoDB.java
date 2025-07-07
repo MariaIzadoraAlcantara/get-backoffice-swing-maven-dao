a6adf425dc44150d718661e97a4d6672e5cbb41e
Tayara Cruz <tayara.gomes@gmail.com>
2025-07-07 21:23:11 +0100

    update readme add e update credenciais para conexão

--- src/main/java/utils/ConexaoDB.java
+++ src/main/java/utils/ConexaoDB.java
@@ -1,19 +1,30 @@
 package utils;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConexaoDB {
-    // URL de ligação à base de dados MySQL
-    private static final String URL = "jdbc:mysql://localhost:3306/ci-java";
-    private static final String USER = "root"; // Substituir pelo seu utilizador
-    private static final String PASSWORD = "*"; // Substituir pela sua password
 
     /**
      * Retorna uma conexão ativa à base de dados.
      */
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        try {
+            Properties props = new Properties();
+            InputStream input = ConexaoDB.class.getClassLoader().getResourceAsStream("config.properties");
+            props.load(input);
+
+            String url = props.getProperty("db.url");
+            String user = props.getProperty("db.user");
+            String password = props.getProperty("db.password");
+
+            return DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
